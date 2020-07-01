92dd87f08213e90deb8f326c8a83cd52fd04708a
Antti Rintala <rintsi@gmail.com>
2020-07-01 13:29:37 +0300

    Export preloaded Package Definitions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// TODO: Make exportable module
+
+const CURRENT_LOOP_PROTO_PATH = __dirname + '/currentLoop.proto';
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
+const packageDefinition = protoLoader.loadSync(
+    CURRENT_LOOP_PROTO_PATH,
+    {
+      keepCase: true,
+      longs: String,
+      enums: String,
+      defaults: true,
+      oneofs: true
+    });
+const CurrentLoopProto = grpc.loadPackageDefinition(packageDefinition);
+
+module.exports = {
+    CurrentLoopProto: CurrentLoopProto
+};
